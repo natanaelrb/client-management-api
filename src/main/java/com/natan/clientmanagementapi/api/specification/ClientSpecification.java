2bdebdafc75028e4d2854eb7627d16f009ca7a17
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-05-06 11:27:10 -0300

    feat: Implements dynamic filters with Specification.

---
+++ src/main/java/com/natan/clientmanagementapi/api/specification/ClientSpecification.java
@@ -0,0 +1,20 @@
+package com.natan.clientmanagementapi.api.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.natan.clientmanagementapi.api.domain.model.Client;
+
+public class ClientSpecification {
+
+    public static Specification<Client> nameContains(String name) {
+        return (root, query, cb) ->
+                name == null ? null :
+                cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%");
+    }
+
+    public static Specification<Client> emailContains(String email) {
+        return (root, query, cb) ->
+                email == null ? null :
+                cb.like(cb.lower(root.get("email")), "%" + email.toLowerCase() + "%");
+    }
+}
